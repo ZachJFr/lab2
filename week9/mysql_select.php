2e017be0d2d2df9e378cb8e481ca39c97cd0b2d2
Your Name <youremail@university.edu>
2023-02-07 15:43:06 +0800

    update 11

--- week9/mysql_select.php
+++ week9/mysql_select.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email " . $row["email"]. " - Website" . $row["website"]."<br>" ;
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email " . $row["email"]. " - Website" . $row["website"]. " - Comment" . $row["comment"]. " - Gender" . $row["gender"]. " - Reg_Date" . $row["reg_date"]."<br>" ;
   }
 } else {
   echo "0 results";
